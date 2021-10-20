414628d0473b299ea5d8c540419cfc186561189f
Brodie Gress <77457950+brodielgress@users.noreply.github.com>
2021-10-20 17:45:27 -0400

    One little indent messing the whole thing up, good grief

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -5,10 +5,10 @@ files = []
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
     valid = lambda p: not isinstance(p, parsers.ResourceParser)
-        for path in source.rglob("*"):
-            for parser in list(filter(valid, site_parsers)):
-                if parser.valid_file_ext(path.suffix):
-                    files.append(path)
+    for path in source.rglob("*"):
+        for parser in list(filter(valid, site_parsers)):
+            if parser.valid_file_ext(path.suffix):
+                files.append(path)
 
 @hooks.register("generate_menu")
 def generate_menu(html, ext):
